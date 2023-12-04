6a1933251cbf6ab65860056823c01b59eb02161f
bashir0vrashad <beshirovreshad777@gmail.com>
2023-12-04 19:52:37 +0400

    clone

---
+++ PrototypeTask/ElectronicDevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeTask
+{
+    public class ElectronicDevice : ICloneableDevice
+    {
+        private string brand;
+        private string model;
+
+        public ElectronicDevice(string brand, string model)
+        {
+            this.brand = brand;
+            this.model = model;
+        }
+
+        public ICloneableDevice Clone()
+        {
+           
+            return new ElectronicDevice(brand, model);
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {brand}, Model: {model}");
+        }
+    }
+}
